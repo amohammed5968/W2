edb823ad8a8ee9fb7eaba680c9f065d1363d352b
ahsannaveedmohammed <mohd.ahsannaveed@gmail.com>
2017-04-18 07:57:58 -0700

    mongodb get and post

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -13,8 +13,13 @@
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
         controllerAs: 'main'
+      })
+      .state('schedule',{
+        url: '/schedule',
+        templateUrl: 'app/components/schedule/schedule.html',
+        controller: 'ScheduleController',
+        controllerAs: 'sch'
       });
-
     $urlRouterProvider.otherwise('/');
   }
 
